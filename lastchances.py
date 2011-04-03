ebe6580913d69ee88e05a3ca465df7f933788dc1
Ian <typppo+github@gmail.com>
2011-04-03 03:02:08 -0400

    changes and form submission

--- lastchances.py
+++ lastchances.py
@@ -38,8 +38,10 @@ class Crush(db.Model):
 class HomeHandler(webapp.RequestHandler):
     def get(self):
         sess = sessions.Session()
+        # TODO show matches so far or other interesting statistics
         args = dict(logged_in=True if 'id' in sess else False)
         self.response.out.write(template.render('index.html', args))
+
 
 class LoginHandler(webapp.RequestHandler):
     def get(self):
@@ -47,10 +49,16 @@ class LoginHandler(webapp.RequestHandler):
         c = CASClient(CAS_URL, SERVICE_URL)
         id = c.Authenticate(self.request.get('ticket', None))
 
-        sess = sessions.Session()
-        sess['id'] = id
-
-        self.redirect('/entry')
+        if id:
+            sess = sessions.Session()
+            try:
+                sess['id'] = id[:id.find('@')].lower()
+                self.redirect('/entry')
+                return
+            except:
+                pass
+        self.response.out.write('Login failed')
+        
 
 class EntryHandler(webapp.RequestHandler):
     def get(self): 
@@ -62,6 +70,7 @@ class EntryHandler(webapp.RequestHandler):
         id = sess['id']
         u = User.get_by_key_name(id)
         if not u:
+            # TODO lookup name and check against list of allowed people
             u = User(key_name=id, id=id)
             u.save()
 
@@ -70,11 +79,26 @@ class EntryHandler(webapp.RequestHandler):
         self.response.out.write(template.render('entry.html', args))
 
     def post(self):
-        pass
+        sess = sessions.Session()
+        if not 'id' in sess:
+            self.response.out.write('You must be logged in')
+            return
+
+        # TODO resolve dnd entries
+
+        c1 = self.request.get('c1')
+        c2 = self.request.get('c2')
+        c3 = self.request.get('c3')
+        c4 = self.request.get('c4')
+        c5 = self.request.get('c5')
+        c6 = self.request.get('c6')
+        c7 = self.request.get('c7')
+        c8 = self.request.get('c8')
+        c9 = self.request.get('c9')
+        c10 = self.request.get('c10')
 
 
 def main():
-    # TODO replace get started with real home page with intro and link to main
     util.run_wsgi_app(webapp.WSGIApplication([
         (r"/", HomeHandler),
         (r"/login", LoginHandler),
